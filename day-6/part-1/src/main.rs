b488171cce98c41b64246261c57a35bba576dc3d
Oliver Wright <meiamsome@meiamso.me>
2019-12-15 08:28:12 +0000

    Day 6; Part 1

---
+++ day-6/part-1/src/main.rs
@@ -0,0 +1,52 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::collections::HashMap;
+
+fn parse_orbit(input: &str) -> (&str, &str) {
+    let split: Vec<&str> = input.split(')').collect();
+    if split.len() != 2 {
+        panic!("Unexpected length of orbit line: {}", split.len());
+    }
+    (split[0], split[1])
+}
+
+fn get_total_orbits(input: &str) -> u32 {
+    let mut planets: HashMap<&str, &str> = HashMap::new();
+    for (parent, child) in input.split('\n').map(parse_orbit) {
+        planets.insert(child, parent);
+    }
+    planets.keys()
+        .map(|planet| {
+            let mut count = 0;
+            let mut current = planet;
+            while let Some(next) = planets.get(current) {
+                current = next;
+                count += 1;
+            }
+            count
+        })
+        .fold(0, |a, b| a + b)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut contents = String::new();
+    {
+        let mut file = File::open("./input.txt")?;
+        file.read_to_string(&mut contents)?;
+    }
+    println!("{}", get_total_orbits(&contents.trim()));
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(
+            42,
+            get_total_orbits("COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L")
+        )
+    }
+}
